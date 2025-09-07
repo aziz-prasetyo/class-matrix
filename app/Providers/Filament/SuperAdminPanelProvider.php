981fba64f64239ad55fcad44bf7d9a87c77b8b40
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2025-09-08 02:21:21 +0700

    feat(theme): integrate Awin Theme with configuration

--- app/Providers/Filament/SuperAdminPanelProvider.php
+++ app/Providers/Filament/SuperAdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Resma\FilamentAwinTheme\FilamentAwinTheme;
 
 class SuperAdminPanelProvider extends PanelProvider
 {
@@ -54,6 +55,9 @@ class SuperAdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentAwinTheme::make(),
             ]);
     }
 }
