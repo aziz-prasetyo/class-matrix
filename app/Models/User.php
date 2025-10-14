b309e0be3e1f40ab1a128cdbdb7d21c3b2b75669
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2025-10-14 08:18:12 +0700

    feat(user): add `LocalizedTimestamp` cast for user timezone handling

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Casts\LocalizedTimestamp;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,8 +43,10 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'email_verified_at' => LocalizedTimestamp::class,
             'password' => 'hashed',
+            'created_at' => LocalizedTimestamp::class,
+            'updated_at' => LocalizedTimestamp::class,
         ];
     }
 }
