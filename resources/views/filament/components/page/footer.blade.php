cea226ced02dc750e7830b432e2cd341c6b706c2
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2025-10-29 20:24:16 +0700

    style: adjust spacing

--- resources/views/filament/components/page/footer.blade.php
+++ resources/views/filament/components/page/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="w-full grid place-items-center bg-primary-50 border-t border-slate-300 dark:bg-primary-950/20 dark:border-slate-700">
     <section class="container px-4 py-6">
-        <div class="flex flex-col justify-between items-center gap-x-8 gap-y-2 text-sm font-medium text-gray-500 sm:flex-row dark:text-white">
+        <div class="flex flex-col justify-between items-center gap-x-8 text-sm font-medium text-gray-500 sm:flex-row dark:text-white">
             <p>© 2025 <x-filament::link href="https://new-fik.upnvj.ac.id" target="_blank" class="underline">FIK UPNVJ</x-filament::link>. All rights reserved.</p>
             <p>
                 Made with 🧡 by <x-filament::link href="https://www.instagram.com/izprstyo" target="_blank">@izprstyo</x-filament::link>
